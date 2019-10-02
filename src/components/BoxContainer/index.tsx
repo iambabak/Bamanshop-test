788b018cf564f4034a652507da45f9e5799f6dc4
Ali Sasani <a.sasani.b@gmail.com>
2019-10-02 13:14:59 +0330

    placing BoxContainer at center of page

--- src/components/BoxContainer/index.tsx
+++ src/components/BoxContainer/index.tsx
@@ -7,7 +7,7 @@ interface FormProps {
 
 const BoxContainer: React.FC<FormProps> = props => {
   return (
-    <div style={{ width: "800px" }}>
+    <div style={{ width: "800px", margin: "30px auto" }}>
       {props.numberOfColumns.map(
         (el, index) => el > 0 && <EachRow key={index} columns={el} />
       )}
